e40c7b7ec7d1786324bc86eb0ad347789bced0c6
Daniel Awale <danielmadhukarawale@gmail.com>
2022-12-26 10:52:15 -0800

    will contain the routes and npm start will work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
-import Create from './pages/Create';
-import Search from './pages/Search';
+// page componenets
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Search from './pages/search/Search';
+import Receipe from './pages/receipe/Receipe';
+
+//styles
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" >
+            <Home />
+          </Route>
+          <Route path="/create" >
+            <Create />
+          </Route>
+          <Route path="/search" >
+            <Search />
+          </Route>
+          <Route path="/receipe/:id" >
+            <Receipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
       
     </div>
   );
