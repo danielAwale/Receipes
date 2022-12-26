b6b918b13c1b681432231661e98ede4b3cbcac32
Daniel Awale <danielmadhukarawale@gmail.com>
2022-12-26 10:37:40 -0800

    import pages and router stuff on the app.js component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Create from './pages/Create';
+import Search from './pages/Search';
 import './App.css'
 
 function App() {
